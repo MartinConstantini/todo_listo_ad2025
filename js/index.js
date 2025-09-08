1fa984b72437bbd16e50cf3776d088e2ac6b747a
MartinConstantini <me.hurtadoconstantini@ugto.mx>
2025-09-08 10:38:22 -0600

    Todo listo

--- js/index.js
+++ js/index.js
@@ -10,24 +10,28 @@ const obtenerTareas = () => {
     return JSON.parse(localStorage.getItem('tareas')) || []
 }
 
-const guardarTareas = (tareas) => {
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-}
-
-const eliminarTarea = (index) => {
-    const tareas = obtenerTareas()
-    tareas.splice(index, 1)
-    guardarTareas(tareas)
-    renderizarTareas()
-}
-
 const renderizarTareas = () => {
     listaTareas.innerHTML = ''
     const tareas = obtenerTareas()
 
-    tareas.forEach((texto, index) => {
+    tareas.forEach((tarea, index) => {
         const nuevaTarea = document.createElement('li')
-        nuevaTarea.textContent = texto
+
+        const spanTexto = document.createElement('span')
+        spanTexto.textContent = tarea.texto
+
+        // Saber si la tarea está completada
+        if(tarea.completada){
+            spanTexto.style.textDecoration = 'line-through'
+            spanTexto.style.color = 'gray'
+        }
+
+        const btnCompletar = document.createElement('button')
+        btnCompletar.textContent = tarea.completada ? '↩' : '✅'
+        btnCompletar.style.marginLeft = '10px'
+        btnCompletar.addEventListener('click', () => {
+            toggleCompletar(index)
+        })
 
         const btnBorrar = document.createElement('button')
         btnBorrar.textContent = '❌'
@@ -36,9 +40,37 @@ const renderizarTareas = () => {
             eliminarTarea(index)
         })
 
+        nuevaTarea.appendChild(spanTexto)
         nuevaTarea.appendChild(btnBorrar)
+        nuevaTarea.appendChild(btnCompletar)
         listaTareas.appendChild(nuevaTarea)
     })
+}
+
+const agregarTarea = texto => {
+    const tareas = obtenerTareas()
+    tareas.push({texto, completada: false})
+    guardarTareas(tareas)
+    renderizarTareas()
+} 
+
+const guardarTareas = tareas => {
+        localStorage.setItem('tareas', JSON.stringify(tareas))
+    }
+
+
+const eliminarTarea = index => {
+    const tareas = obtenerTareas()
+    tareas.splice(index, 1)
+    guardarTareas(tareas)
+    renderizarTareas()
+}
+
+const toggleCompletar = (index) => {
+    const tareas = obtenerTareas()
+    tareas[index].completada = !tareas[index].completada
+    guardarTareas(tareas)
+    renderizarTareas()
 }
 
 btnCambiar.addEventListener('click', () =>{
@@ -52,12 +84,29 @@ btnAgregar.addEventListener('click', () => {
         return
     }
 
-    const tareas = obtenerTareas()
-    tareas.push(texto)
-    guardarTareas(tareas)
-    renderizarTareas()
+    agregarTarea(texto)
     inputTarea.value = ''
+
 })
 
-// Render inicial al cargar
-renderizarTareas()
+document.addEventListener('DOMContentLoaded', () => {
+        renderizarTareas()
+    })
+
+
+/*
+
+    const nuevaTarea = document.createElement('li')
+    nuevaTarea.textContent = texto
+
+    const btnBorrar = document.createElement('button')
+    btnBorrar.textContent = '❌'
+    btnBorrar.style.marginLeft = '10px'
+    btnBorrar.addEventListener('click', () => {
+        listaTareas.removeChild(nuevaTarea)
+    })
+
+    nuevaTarea.appendChild(btnBorrar)
+    listaTareas.appendChild(nuevaTarea)
+
+*/
